9f4c6da6bd3978c367c4398a6104f45a7ceb11ed
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-22 01:11:32 +0500

    Add files via upload

---
+++ question5/main5.js
@@ -0,0 +1,3 @@
+var famous_person = "Albert Einstein";
+var person_msg = "once said, A person who never made a mistake never tried anything new";
+console.log("our famous", famous_person, "he said", person_msg);
---
+++ question5/main5.ts
@@ -0,0 +1,3 @@
+let famous_person="Albert Einstein";
+let person_msg="once said, A person who never made a mistake never tried anything new..";
+console.log ("our famous",famous_person ,"he said" ,person_msg);
