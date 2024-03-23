d9a54a64b44942d0ac727cf0a303c55a5164a090
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-23 16:01:42 +0500

    Add files via upload

---
+++ question14/index.js
@@ -0,0 +1,4 @@
+var guestlist = ["Salman", "faizi", "aliza"];
+guestlist.forEach(function (guest) {
+    console.log("salam ".concat(guest, " I would like to invite for dinner "));
+});
---
+++ question14/index.ts
@@ -0,0 +1,4 @@
+let guestlist=["Salman","faizi","aliza"];
+guestlist.forEach(function(guest){
+    console.log(`salam ${guest} I would like to invite for dinner `);
+})
