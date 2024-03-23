42642f140c1eace19f03064c386cc66d17442028
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-23 15:56:04 +0500

    Add files via upload

---
+++ question13/index.js
@@ -0,0 +1,4 @@
+var transportation = ["sports cars", "lamborghini car", "Sports Bike"];
+transportation.forEach(function (mode) {
+    console.log("I would like to own a ".concat(mode));
+});
---
+++ question13/index.ts
@@ -0,0 +1,4 @@
+let transportation=["sports cars","lamborghini car","Sports Bike"];
+transportation.forEach(function(mode){
+    console.log(`I would like to own a ${mode}`);
+})
