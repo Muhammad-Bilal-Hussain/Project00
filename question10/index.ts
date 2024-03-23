42c2786993ce8d4b09dbadd0701640576eafc175
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-23 15:37:01 +0500

    Add files via upload

---
+++ question10/index.ts
@@ -0,0 +1,11 @@
+//this print for addition
+console.log(4+4);
+
+//this print for Subtraction
+console.log(10-2);
+
+//this print for Multiplication
+console.log(2*4);
+
+//this print for divide
+console.log(16/2);
