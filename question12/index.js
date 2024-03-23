48e7ac67eaf64d8459ac68c0269b7c985db73c85
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-23 15:39:24 +0500

    Add files via upload

---
+++ question12/index.js
@@ -0,0 +1,4 @@
+var friendName = ["Salman", "Hammad", "Muzammil"];
+friendName.forEach(function (friendName) {
+    console.log("hello ".concat(friendName, " How r u"));
+});
---
+++ question12/index.ts
@@ -0,0 +1,4 @@
+let friendName =["Salman","Hammad","Muzammil"];
+friendName.forEach(function(friendName){
+    console.log(`hello ${friendName} How r u`);
+})
