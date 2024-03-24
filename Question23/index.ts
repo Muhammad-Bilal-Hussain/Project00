9abd78b4cc3925907207a0e9005d2b05993eeb36
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-25 03:59:25 +0500

    Add files via upload

---
+++ Question23/index.ts
@@ -0,0 +1,36 @@
+let car= "sabaru";
+let bike= "Suzuki";
+//true condition
+//test 1
+console.log("=========True Condition========\n");
+console.log("car is sabaru");
+console.log(car == "sabaru" );
+//test 2
+console.log("\ncar is sabaru not bike");
+console.log(car != bike );
+//test 3
+console.log("\ncar is sabaru beautifull");
+console.log(car == "sabaru" );
+//test 4
+console.log("\ncar is sabaru not damage");
+console.log(car == "sabaru" );
+//test 5
+console.log("\ncar is sabaru heavy from bike");
+console.log(car === "sabaru" ,"\n");
+//false conditons
+//test 6
+console.log("\n========Now False Condition=======");
+console.log("\nBike is sabaru");
+console.log(bike !== bike );
+//test 7
+console.log("\nbike is Suzuki not car");
+console.log(bike > bike );
+//test 8
+console.log("\nbike is not beautifull");
+console.log(bike < bike);
+//test 9
+console.log("\nbike suzuki is damage for car");
+console.log(bike == car );
+//test 10
+console.log("\nbike is sabaru heavy from car");
+console.log(car === "Suzuki" );
