904b0006682591dbed6194947ca2df4328bb55c4
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-24 15:28:32 +0500

    Add files via upload

---
+++ Question19/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("../question14/main");
+// let invitepeople_count: number = invitedpeople.length;
+console.log("".concat(main_1.guestlist.length, " people will be come to dinner"));
---
+++ Question19/index.ts
@@ -0,0 +1,3 @@
+import{guestlist} from '../question14/main'
+// let invitepeople_count: number = invitedpeople.length;
+console.log(`${guestlist.length} people will be come to dinner`);
