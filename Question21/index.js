d2235c5993fe72a51f6a86910128c559a54527a8
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-25 03:55:48 +0500

    Add files via upload

---
+++ Question21/index.js
@@ -0,0 +1,8 @@
+;
+var Student = {
+    student_Name: "M Bilal Hussain",
+    Sfather_Name: "Dilshad hussain",
+    Course: "Intiative A.I",
+    Timing: 2 - .4
+};
+console.log(Student);
---
+++ Question21/index.ts
@@ -0,0 +1,14 @@
+// Define a TypeScript interface for a book
+interface Student {
+    student_Name: string,
+    Sfather_Name: string,
+    Course: string,
+    Timing: number,
+};
+let Student = {
+    student_Name:"M Bilal Hussain",
+    Sfather_Name:"Dilshad hussain",
+    Course:"Intiative A.I",
+    Timing: 2.4
+}
+console.log(Student);
