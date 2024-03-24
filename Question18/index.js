659b0d27c745ffa5bc561f74cec21b6c3fb42b7e
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-24 15:26:50 +0500

    Add files via upload

---
+++ Question18/index.js
@@ -0,0 +1,10 @@
+var world_seeing = ['Brazil', 'Australia', 'USA', 'Canada', 'Dubai'];
+console.log(world_seeing);
+world_seeing.sort();
+console.log(world_seeing);
+world_seeing.reverse();
+console.log(world_seeing);
+world_seeing.reverse();
+console.log(world_seeing);
+world_seeing.sort();
+console.log(world_seeing);
---
+++ Question18/index.ts
@@ -0,0 +1,20 @@
+let world_seeing : string[] = ['Brazil', 'Australia' , 'USA' , 'Canada' , 'Dubai'];
+
+console.log(world_seeing);
+
+world_seeing.sort()
+
+console.log(world_seeing);
+
+
+world_seeing.reverse();
+
+console.log(world_seeing);
+
+world_seeing.reverse();
+
+console.log(world_seeing);
+
+world_seeing.sort();
+
+console.log(world_seeing);
