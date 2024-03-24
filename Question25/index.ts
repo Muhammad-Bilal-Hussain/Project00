e3e94aace4dedd54618763222ea304602f4a77ae
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-25 04:03:11 +0500

    Add files via upload

---
+++ Question25/index.ts
@@ -0,0 +1,7 @@
+let alienColor = "Gray";
+if (alienColor==="Gray") {
+    console.log("the player just earned 5 points")
+}
+ if(alienColor==="white"){
+    console.log("The version that fails will have no output.")
+};
