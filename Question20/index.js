24ca15867db3828d24f6f342d6eecb262b0ea43c
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-24 15:30:12 +0500

    Add files via upload

---
+++ Question20/index.js
@@ -0,0 +1,8 @@
+var languages = ['Urdu', 'punjabi', 'Sindhi', 'Balochi', 'pakhton'];
+// console.log('List of languages \n');
+// for( let i = 0 ; i<languages.length ; i++) {
+//     console.log(`${languages[i]}`);
+// }
+languages.forEach(function (language) {
+    console.log("".concat(language, " This is Pakilstani Language"));
+});
---
+++ Question20/index.ts
@@ -0,0 +1,9 @@
+let languages : string[] = ['Urdu','punjabi','Sindhi','Balochi','pakhton'];
+
+// console.log('List of languages \n');
+// for( let i = 0 ; i<languages.length ; i++) {
+//     console.log(`${languages[i]}`);
+// }
+languages.forEach(function(language){
+    console.log(`${language} This is Pakilstani Language`);
+});
