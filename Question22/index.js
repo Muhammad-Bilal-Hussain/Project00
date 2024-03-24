24082d0acd3045e6b4159cf2d1da17e9cc379f79
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-25 03:57:34 +0500

    Add files via upload

---
+++ Question22/index.js
@@ -0,0 +1,4 @@
+var error = ['11', '22', '33', '44'];
+//error undifined
+//console.log(error[4]);
+console.log(error[0]);
---
+++ Question22/index.ts
@@ -0,0 +1,4 @@
+let error: string[] = ['11','22','33','44'];
+//error undifined
+//console.log(error[4]);
+console.log(error[0]);
