415f64f06519c6476019c132ce0ae02b517a7181
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-26 14:58:57 +0500

    Add files via upload

---
+++ Question30/index.ts
@@ -0,0 +1,37 @@
+let form=["Admin","Sauod","Admin","Hassan","Jawwad","Maaz"];
+if (form.includes("Admin1")){
+    console.log("Hello admin, would you like to see a status report?");
+} 
+else{
+    console.log("Hello humen, thank you for logging in again.");
+}
+if (form.includes("Sauod bhai")){
+    console.log("Hello Sauod, would you like to see a status report?");
+} 
+else{
+    console.log("Hello Human, thank you for logging in again.");
+}
+if (form.includes("Admin2")){
+    console.log("Hello admin, would you like to see a status report?");
+} 
+else{
+    console.log("Hello Humen, thank you for logging in again.");
+}
+if (form.includes("Hassan")){
+    console.log("Hello Hassan, would you like to see a status report?");
+} 
+else{
+    console.log("Hello human, thank you for logging in again.");
+}
+if (form.includes("Jawwad")){
+    console.log("Hello Jawwad, would you like to see a status report?");
+} 
+else{
+    console.log("Hello Human, thank you for logging in again.");
+}
+if (form.includes("Maaz")){
+    console.log("Hello Maaz, would you like to see a status report?");
+} 
+else{
+    console.log("Hello Human, thank you for logging in again.");
+}
