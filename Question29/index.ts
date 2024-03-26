4dc7483fc6bc955170bb0760d1dd4653dbe7cda5
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-26 14:57:07 +0500

    Add files via upload

---
+++ Question29/index.ts
@@ -0,0 +1,27 @@
+// Define array of favorite fruits
+let favorite_fruits: string[] = ["apple", "banana", "mango"];
+
+// Check if each fruit is in the array of favorite fruits
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes!");
+}
+
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+} else {
+    console.log("Oranges are not one of your favorite fruits.");
+}
+
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+} else {
+    console.log("Strawberries are not one of your favorite fruits.");
+}
