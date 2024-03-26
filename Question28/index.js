7fe66d91411923d362e40e95039709338df75774
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-26 14:55:35 +0500

    Add files via upload

---
+++ Question28/index.js
@@ -0,0 +1,19 @@
+var personAge = 0;
+if (personAge <= 2) {
+    console.log("its baby");
+}
+else if (personAge >= 2 && personAge < 4) {
+    console.log("its toddler");
+}
+else if (personAge >= 4 && personAge < 13) {
+    console.log("its kid");
+}
+else if (personAge >= 13 && personAge < 20) {
+    console.log("its Teenager");
+}
+else if (personAge >= 20 && personAge < 65) {
+    console.log("its Adult");
+}
+else {
+    console.log("Now he is Elder");
+}
