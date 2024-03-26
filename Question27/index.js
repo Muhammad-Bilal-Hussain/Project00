f7f7ade2ee9ab537ec0840cc6078f8c506758856
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-26 14:53:18 +0500

    Add files via upload

---
+++ Question27/index.js
@@ -0,0 +1,33 @@
+var alienColor = "Green";
+if (alienColor === "Green") {
+    console.log("(virsion 1)Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alienColor === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alienColor === "Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+//2nd virsion
+var alienColor2 = "Yellow";
+if (alienColor2 === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alienColor2 === "Yellow") {
+    console.log("(virsion 2)Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alienColor2 === "Red") {
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+;
+var alienColor3 = "Red";
+if (alienColor3 === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alienColor3 === "Yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alienColor3 === "Red") {
+    console.log("(virsion 3)Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+;
---
+++ Question27/index.ts
@@ -0,0 +1,28 @@
+let alienColor = "Green";
+
+if (alienColor==="Green") {
+    console.log("(virsion 1)Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alienColor === "yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alienColor === "Red"){
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.");
+}
+//2nd virsion
+let alienColor2 = "Yellow";
+
+if (alienColor2 ==="green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.")
+} else if (alienColor2 === "Yellow") {
+    console.log("(virsion 2)Congratulations! You just earned 10 points for shooting the yellow alien.")
+} else if (alienColor2 === "Red"){
+    console.log("Congratulations! You just earned 15 points for shooting the Red alien.")
+};
+let alienColor3 = "Red";
+
+if (alienColor3 ==="green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.")
+} else if (alienColor3 === "Yellow") {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.")
+} else if (alienColor3 === "Red"){
+    console.log("(virsion 3)Congratulations! You just earned 15 points for shooting the Red alien.")
+};
