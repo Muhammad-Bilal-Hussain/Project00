b70a8d65d5e5c6d6a2ae69c92a6bc997675ae979
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-03-26 14:50:50 +0500

    Add files via upload

---
+++ Question26/index.js
@@ -0,0 +1,15 @@
+var alienColor = "Green";
+if (alienColor === "Green") {
+    console.log(" the player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("the player just earned 10 points..");
+}
+;
+// third virsion
+// let alienColor = 'Red';
+// if (alienColor === "Green") {
+//     console.log("the player just earned 5 points for shooting the alien")
+// } else {
+//     console.log("the player just earned 10 points..")
+// };
