7e9cfe94866864286e36a8370fd324d27dd19214
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-04-04 02:26:41 +0500

    Add files via upload

---
+++ Question34/index.js
@@ -0,0 +1,6 @@
+var pizza = ["fajita", "afghani", "cheese", "pepperoni"];
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var one_pizza = pizza_1[_i];
+    console.log("I like ".concat(one_pizza, " pizza."));
+}
+console.log(" I really love pizza!");
---
+++ Question34/index.ts
@@ -0,0 +1,5 @@
+let pizza=["fajita","afghani","cheese","pepperoni"]
+ for(let one_pizza of pizza){
+    console.log(`I like ${one_pizza} pizza.`)
+ }
+ console.log(" I really love pizza!")
