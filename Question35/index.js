7156cd3188e9b7e61d87b71c13c0a041b323fc41
Muhammad-Bilal-Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-04-04 02:29:00 +0500

    Add files via upload

---
+++ Question35/index.js
@@ -0,0 +1,7 @@
+var petAnimal = ["Dog", "Lion", "Cat", "Cow"];
+for (var _i = 0, petAnimal_1 = petAnimal; _i < petAnimal_1.length; _i++) {
+    var onepetAnimal = petAnimal_1[_i];
+    console.log(" A ".concat(onepetAnimal, " would make a great pet."));
+}
+// output print a msg
+console.log("Any of these animals would make a great pet!");
---
+++ Question35/index.ts
@@ -0,0 +1,7 @@
+let petAnimal=["Dog","Lion","Cat","Cow"]
+
+for(let onepetAnimal of petAnimal){
+    console.log(` A ${onepetAnimal} would make a great pet.`);
+}
+// output print a msg
+console.log("Any of these animals would make a great pet!")
