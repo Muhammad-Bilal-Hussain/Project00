6bd2e4722f1128b64bbacbfc22fd7309a2babdb2
Muhammad Bilal Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-04-15 02:51:53 +0500

    Update index.ts

--- Question45/index.ts
+++ Question45/index.ts
@@ -16,19 +16,3 @@ return car;
 }
 let myCar = creat_Cars('Honda Civic','FE4' ,'Color: Red', "Engine: Gasoline","Battery: Lithium-ion (e_HEV)");
 console.log(myCar)
-// interface CarInformation {manufacturer: string,model: string,[key: string]: any;}
-
-// function make_cars(manufacturer : string , model : string , ...options: [string, any[]]) : CarInformation {
-//     const into : CarInformation = {
-//         manufacturer,
-//         model,
-//     };
-
-//     for (const[key , value] of options) {
-//         into[key] = value;
-//     }
-//     return into 
-// }
-
-// const information : string = make_cars('Honda Civic','FE4' ,['Color','Black'], ['Engine','Gasoline'],['Battery','Lithium-ion (e:HEV)']);
-// console.log(information);
