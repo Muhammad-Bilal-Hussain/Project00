693be0438f2a0f6a9d53520eda0ad1fd79592fea
Muhammad Bilal Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-04-15 02:15:14 +0500

    Add files via upload

---
+++ Question41/index.js
@@ -0,0 +1,5 @@
+function show_magicians(megicians) {
+    megicians.forEach(function (magician_Name) { return console.log(magician_Name); });
+}
+var nameList = ["Kashif", "Bilal", "Farax"];
+show_magicians(nameList);
---
+++ Question41/index.ts
@@ -0,0 +1,5 @@
+function show_magicians (megicians:string[]){
+    megicians.forEach(magician_Name => console.log(magician_Name));
+}
+let nameList = ["Kashif","Bilal","Farax"];
+show_magicians(nameList)
