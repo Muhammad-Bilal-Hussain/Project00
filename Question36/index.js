6410eefe156c4e41ee77a328c3746a6edef4cb4b
Muhammad Bilal Hussain <143377784+Muhammad-Bilal-Hussain@users.noreply.github.com>
2024-04-15 02:03:47 +0500

    Add files via upload

---
+++ Question36/index.js
@@ -0,0 +1,5 @@
+function make_shirt(size, printmsg) {
+    console.log("My Shirt Size Is ".concat(size, " And Print Message on This ").concat(printmsg));
+}
+make_shirt("Small", "'CODER'...");
+make_shirt("MEDIUM", "'CODER'...");
---
+++ Question36/index.ts
@@ -0,0 +1,5 @@
+function make_shirt(size:string, printmsg:string){
+console.log(`My Shirt Size Is ${size} And Print Message on This ${printmsg}`)
+}
+make_shirt("Small","'CODER'...");
+make_shirt("MEDIUM","'CODER'...");
